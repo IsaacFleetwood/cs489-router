e1e76b76d233c6a69873d04d15e33f45e9abbc39
HyunShu <hyunshu@hotmail.com>
2025-03-11 17:02:11 +0000

    dhcp minor changes

--- router/include/dhcp.h
+++ router/include/dhcp.h
@@ -8,6 +8,15 @@
 #define DHCP_SERVER_PORT 67
 #define DHCP_CLIENT_PORT 68
 #define DHCP_BUFFER_SIZE 1024
+#define MAX_LEASES 10
+#define DHCP_OPTION_MESSAGE_TYPE 53
+#define DHCP_OPTION_REQUESTED_IP 50
+#define DHCP_DISCOVER 1
+#define DHCP_OFFER 2
+#define DHCP_REQUEST 3
+#define DHCP_ACK 5
+#define DHCP_NAK 6
+#define DHCP_END 255
 
 // Function Declarations
 void dhcp_server_start();  // Starts the DHCP server
--- router/src/dhcp.c
+++ router/src/dhcp.c
@@ -8,25 +8,15 @@
 #include <stdint.h>
 #include "../include/dhcp.h"
 
-#define MAX_LEASES 10
-#define DHCP_OPTION_MESSAGE_TYPE 53
-#define DHCP_OPTION_REQUESTED_IP 50
-#define DHCP_DISCOVER 1
-#define DHCP_OFFER 2
-#define DHCP_REQUEST 3
-#define DHCP_ACK 5
-#define DHCP_NAK 6
-#define DHCP_END 255
-
 typedef struct {
-    struct in_addr ip;      // Assigned IP
-    uint8_t mac[6];         // Client MAC Address
-    time_t lease_start;     // Lease start time
-    time_t lease_expiry;    // Lease expiration time
-    int allocated;          // 1 if assigned, 0 if available
+    struct in_addr ip;
+    uint8_t mac[6];
+    time_t lease_start;
+    time_t lease_expiry;
+    int allocated;
 } dhcp_lease_t;
 
-static dhcp_lease_t lease_table[MAX_LEASES];  // DHCP lease table
+static dhcp_lease_t lease_table[MAX_LEASES];
 
 struct in_addr dhcp_pool_start;
 struct in_addr dhcp_pool_end;
@@ -50,11 +40,8 @@ struct in_addr extract_requested_ip(uint8_t* buffer);
 void send_dhcp_offer(int sockfd, struct sockaddr_in* client_addr, uint8_t* buffer);
 void send_dhcp_ack(int sockfd, struct sockaddr_in* client_addr, struct in_addr allocated_ip);
 
-/**
- * Start the DHCP server, listening on UDP port 67
- */
 void dhcp_server_start() {
-    init_dhcp_pool();  // **Initialize IP pool before using it!**
+    init_dhcp_pool();
 
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
@@ -76,7 +63,7 @@ void dhcp_server_start() {
         exit(EXIT_FAILURE);
     }
 
-    printf("DHCP Server running on port %d...\n", DHCP_SERVER_PORT);
+    printf("DHCP Server running on port %d\n", DHCP_SERVER_PORT);
 
     socklen_t len = sizeof(client_addr);
     while (1) {
@@ -88,14 +75,11 @@ void dhcp_server_start() {
     close(sockfd);
 }
 
-/**
- * Handles DHCP Discover and DHCP Request messages (DORA process)
- */
 void handle_dhcp_packet(int sockfd, struct sockaddr_in* client_addr, uint8_t* buffer, int recv_len) {
-    uint8_t* options = buffer + 240;    // Skip Ethernet and IP headers
+    uint8_t* options = buffer + 240;
     uint8_t message_type = 0;
 
-    while (*options != DHCP_END) {
+    while (*options != DHCP_END && options < buffer + recv_len) {
         uint8_t option_type = options[0];
         uint8_t option_len = options[1];
 
@@ -107,25 +91,22 @@ void handle_dhcp_packet(int sockfd, struct sockaddr_in* client_addr, uint8_t* bu
         options += 2 + option_len;
     }
 
-    if (message_type == DHCP_DISCOVER) {                                                        // D : DHCP Discover
+    if (message_type == DHCP_DISCOVER) {
         printf("Received DHCP Discover from %s\n", inet_ntoa(client_addr->sin_addr));
-        send_dhcp_offer(sockfd, client_addr, buffer);                                           // O : Send DHCP Offer
-    } else if (message_type == DHCP_REQUEST) {                                                  // R : DHCP Request
+        send_dhcp_offer(sockfd, client_addr, buffer);
+    } else if (message_type == DHCP_REQUEST) {
         printf("Received DHCP Request from %s\n", inet_ntoa(client_addr->sin_addr));
         struct in_addr requested_ip = extract_requested_ip(buffer);
-        send_dhcp_ack(sockfd, client_addr, requested_ip);                                       // A : Send DHCP ACK
+        send_dhcp_ack(sockfd, client_addr, requested_ip);
     }
 }
 
-/**
- * Finds and returns an available IP address from the DHCP pool
- */
 struct in_addr get_available_ip() {
     for (int i = 0; i < MAX_LEASES; i++) {
         if (!lease_table[i].allocated) {
             lease_table[i].allocated = 1;
             lease_table[i].lease_start = time(NULL);
-            lease_table[i].lease_expiry = lease_table[i].lease_start + 1800;        // 30 min lease
+            lease_table[i].lease_expiry = lease_table[i].lease_start + 1800;
             lease_table[i].ip.s_addr = htonl(ntohl(dhcp_pool_start.s_addr) + i);
             return lease_table[i].ip;
         }
@@ -134,14 +115,11 @@ struct in_addr get_available_ip() {
     return empty;
 }
 
-/**
- * Extracts the requested IP address from a DHCP Request packet
- */
 struct in_addr extract_requested_ip(uint8_t* buffer) {
     struct in_addr requested_ip = { .s_addr = 0 };
     uint8_t* options = buffer + 240;
 
-    while (*options != DHCP_END) {
+    while (*options != DHCP_END && options < buffer + DHCP_BUFFER_SIZE) {
         uint8_t option_type = options[0];
         uint8_t option_len = options[1];
 
@@ -156,9 +134,6 @@ struct in_addr extract_requested_ip(uint8_t* buffer) {
     return requested_ip;
 }
 
-/**
- * Sends a DHCP Offer to the client
- */
 void send_dhcp_offer(int sockfd, struct sockaddr_in* client_addr, uint8_t* buffer) {
     struct in_addr offered_ip = get_available_ip();
     printf("Sending DHCP Offer (IP: %s)\n", inet_ntoa(offered_ip));
@@ -181,13 +156,12 @@ void send_dhcp_offer(int sockfd, struct sockaddr_in* client_addr, uint8_t* buffe
     sendto(sockfd, offer_packet, sizeof(offer_packet), 0, (struct sockaddr*)client_addr, sizeof(*client_addr));
 }
 
-/**
- * Sends a DHCP Acknowledgment (ACK) to the client
- */
 void send_dhcp_ack(int sockfd, struct sockaddr_in* client_addr, struct in_addr allocated_ip) {
     printf("Sending DHCP ACK (IP: %s)\n", inet_ntoa(allocated_ip));
 
     uint8_t ack_packet[DHCP_BUFFER_SIZE] = {0};
+    memcpy(ack_packet, client_addr, sizeof(struct sockaddr_in)); // Ensure correct client data
+
     uint8_t* options = ack_packet + 240;
 
     *options++ = DHCP_OPTION_MESSAGE_TYPE;
